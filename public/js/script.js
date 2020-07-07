2e32c338ba50e6a7a3078f8fc85859e55abf0acf
Bus74 <athenyan74@hotmail.com>
2020-07-07 16:12:44 +0200

    navbar et footer fonctionels

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+$(".button").click(function(){
+    $(".social.twitter").toggleClass("clicked");
+    $(".social.facebook").toggleClass("clicked");
+    $(".social.instagram").toggleClass("clicked");
+    $(".social.youtube").toggleClass("clicked");
+  })
