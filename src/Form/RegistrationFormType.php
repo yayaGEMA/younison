d7aa736dbf3673fac25eb207f530602710d198e6
GROSJEAN <loicgrosjean96@gmail.com>
2020-07-08 15:12:16 +0200

    Ajout formulaire et fichier bundles et fichiers composer.json et composer.lock

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,44 +4,85 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudonyme',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new NotBlank([
+                        'message' => 'Merci de renseigner un pseudonyme'
                     ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre pseudonyme doit contenir au moins {{ limit }} caractères',
+                        'max' => 40,
+                        'maxMessage' => 'Votre pseudonyme doit contenir au maximum {{ limit }} caractères'
+                    ]),
+                ]
+            ])
+            // Champ email
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'constraints' => [
+                    new Email([
+                        'message' => 'L\'adresse email {{ value }} n\'est pas une adresse valide'
+                    ]),
+                    new NotBlank([
+                        'message' => 'Merci de renseigner une adresse email'
+                    ])
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe ne correspond pas à sa confirmation',
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe'
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Merci de renseigner un mot de passe'
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 60,
+                        'maxMessage' => 'Mot de passe trop grand'
                     ]),
-                ],
+                    new Regex([
+                        'pattern' => "/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ !\"\#\$%&\'\(\)*+,\-.\/:;<=>?@[\\^\]_`\{|\}~])^.{8,60}$/",
+                        'message' => 'Votre mot de passe doit contenir obligatoirement une minuscule, une majuscule, un chiffre et un caractère spécial'
+                    ])
+                ]
             ])
+
+            // Bouton de validation
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer mon compte',
+                'attr' => [
+                    'class' => 'btn btn-outline-primary col-12'
+                ]
+            ])
+            ->add('roles')
+            ->add('profilPic')
+            ->add('registrationDate')
+            ->add('isActivated')
+            
+            ->add('isVerified')
         ;
     }
 
