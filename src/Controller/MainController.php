594a2a89c69310d7b8b4d20e3f0c63ef133b4624
yayaGEMA <marceaugerard1610@gmail.com>
2020-07-08 11:59:49 +0200

    Créé système d'Authentification

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("", name="main")
+     * @Route("home", name="main")
      */
     public function index()
     {
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,14 +3,17 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -25,26 +28,21 @@ class User
     private $pseudo;
 
     /**
-     * @ORM\Column(type="string", length=320)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=60)
+     * @ORM\Column(type="json")
      */
-    private $password;
+    private $roles = [];
 
     /**
      * @ORM\Column(type="string", length=50, nullable=true)
      */
     private $profilPic;
 
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $role = [];
-
-    /**
+        /**
      * @ORM\Column(type="datetime")
      */
     private $registrationDate;
@@ -69,6 +67,17 @@ class User
         $this->articles = new ArrayCollection();
         $this->comments = new ArrayCollection();
     }
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -99,9 +108,41 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->password;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -109,6 +150,23 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getProfilPic(): ?string
@@ -119,18 +177,6 @@ class User
     public function setProfilPic(?string $profilPic): self
     {
         $this->profilPic = $profilPic;
-
-        return $this;
-    }
-
-    public function getRole(): ?array
-    {
-        return $this->role;
-    }
-
-    public function setRole(array $role): self
-    {
-        $this->role = $role;
 
         return $this;
     }
@@ -220,4 +266,27 @@ class User
 
         return $this;
     }
+
+    /**
+     * fonction toString
+     */
+    public function __toString() {
+
+        // Le return doit renvoyer quelque chose permettant d'identifier facilement l'élément en question
+        // Ici ça retournera une phrase type "25 - bob@exemple.com"
+        return $this->id . ' - ' . $this->email;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
 }
+
