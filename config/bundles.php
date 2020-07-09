d97e8c0e10bd8c4c3c30f989a519787c14257f20
yayaGEMA <marceaugerard1610@gmail.com>
2020-07-09 10:22:44 +0200

    Remise à neuf - initial

--- config/bundles.php
+++ config/bundles.php
@@ -12,19 +12,12 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-<<<<<<< HEAD
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-=======
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> efb7fe1de99a78d1b1fd482d605d0ca64f6166b5
-=======
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
->>>>>>> cf5bb259c4c0df19e933cdd5980d5cfe3b31628d
->>>>>>> cb86690ba770b82575d90c9968e95e6b78ea02a6
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
 ];
--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-$(".button").click(function(){
-    $(".social.twitter").toggleClass("clicked");
-    $(".social.facebook").toggleClass("clicked");
-    $(".social.instagram").toggleClass("clicked");
-    $(".social.youtube").toggleClass("clicked");
-  })
